f710e57e4630aa0abfa1d43d39886f1abf2a4e36
Rashi Sahu <Rashi.Sahu@lntinfotech.com>
2020-02-03 17:24:56 +0530

    Getting the product list

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
 import Product from "./Product";
 import Title from "./Title";
+import {storeProducts} from "../data"
 class ProductList extends Component {
+    state={
+        products: storeProducts
+    };  
     render() {
+        console.log(this.state.products); 
         return (
             <React.Fragment>
                 <div className="py-5">
